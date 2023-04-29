60ad0a5f7b5a4cae1e54cd8594291fdc1be31b32
sweeta2022 <lourdupaulet.sweeta@stratos.studio>
2023-04-29 12:00:38 +0530

    React - pagination problem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todosPerPage,setTodosPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const numberOfTotalPages = Math.ceil(todos.length/todosPerPage);
+
+  const pages = [...Array(numberOfTotalPages+1).keys()].slice(1);
+
+  console.log(pages);
+  
+  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+
+  const visiblePages = todos.slice(indexOfFirstTodo,indexOfLastTodo);
+
+  const prevHandler = () =>{
+    if(currentPage!==1)
+    {
+      setCurrentPage(currentPage-1);
+    }    
+  };
+  const nextHandler = () => {
+    if(currentPage!==numberOfTotalPages)
+    {
+      setCurrentPage(currentPage+1);
+    }
+  };
+
+  const selectPageHandler = (e) => {
+    console.log("selectPageHandler = "+e.target.value);
+    setTodosPerPage(e.target.value);
+  }
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+    .then((result)=>{
+      setTodos(result.data);
+    })
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <select onChange={selectPageHandler}>
+          <option value="10">10</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
+        </select>
+      <h5>
+        <span onClick={prevHandler}>Prev | </span>
+        {pages.map((page)=>{
+        return <span key={page} onClick={()=>{setCurrentPage(page);}} className={`${(currentPage===page) ?"active":""}`} >{`${page} | `}</span>
+      })} 
+       <span onClick={nextHandler}>Next</span>
+      </h5> 
+      {
+      visiblePages.map((todo)=>{
+          return <p key ={todo.id}>{todo.title}</p>
+      })
+      }
+     </div>  
   );
 }
 
